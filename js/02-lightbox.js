c791aa5013659bf5ecce5c8030a59ac6370071e2
AdamenkoVadim <106696623+AdamenkoVadim@users.noreply.github.com>
2023-01-23 22:39:02 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const getGallery = document.querySelector('.gallery');
+const marcapGallery = markGalery(galleryItems);
+
+getGallery.insertAdjacentHTML('afterbegin', marcapGallery);
+
+function markGalery(elem) {
+    
+    return elem.map(({ preview, original, description }) => {
+			return `<li>
+                  <a class="gallery__item" href="${original}">
+                     <img
+                       class="gallery__image"
+                       src="${preview}"
+                       alt="${description}"   
+                       />
+                       </a>
+                       </li> `;  })	.join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250, });
+
